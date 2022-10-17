31f18418745c22c4e4d5b1bd8e20753a5b54fe77
Priti Yadav <pritiyadav888.com>
2022-10-17 16:21:38 -0700

    first commit (Homepage and app icon)

---
+++ LastCall/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const axios = require('axios');
+import Home from './screens/Home.js';
+const App = () => {
+  return <Home />;
+};
+export default App;
+
+
---
+++ LastCall/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import HomeComponent from '../components/Home.js';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HomeComponent />
+    </SafeAreaView>
+  );
+}
