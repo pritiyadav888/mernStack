a3dbb56a01bb04c8e13661d4ae447c93b3dae2ce
Priti Yadav <pritiyadav888.com>
2022-11-01 12:44:21 -0700

    user authentication added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel");
+const { hashPassword } = require("../utils/hashPassword");
+const generateAuthToken = require("../utils/generateAuthToken")
 
 const getUsers = async (req, res, next) => {
   try {
@@ -9,53 +11,45 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-// const registerUser = async (req, res, next) => {
-//   try {
-//     const { name, lastName, email, password } = req.body;
-//     if (!(name && lastName && email && password)) {
-//       return res.status(400).send("All inputs are required");
-//     }
-
-//     const userExists = await User.findOne({ email });
-//     if (userExists) {
-//       return res.status(400).json({ error: "user exists" });
-//     } else {
-//       const user = await User.create({
-//         name,
-//         lastName,
-//         email: email.toLowerCase(),
-//         password: password
-//       });
-//       res.status(201).send(user)
-//     }
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
 const registerUser = async (req, res, next) => {
   try {
-    const {name, lastName, email, password} = req.body
-    if(!(name && lastName && email && password)){
-      return res.status(400).send("All inputs are required!")
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
     }
-    const userExists = await User.findOne({email});
-    if(userExists){
-      return res.status(400).json({error:"User exists"});
-    }else{
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).send("user exists");
+    } else {
+      const hashedPassword = hashPassword(password);
       const user = await User.create({
         name,
         lastName,
-        email:email.toLowerCase(),
-        password:password
+        email: email.toLowerCase(),
+        password: hashedPassword,
       });
-      res.status(201).send(user)
+      res
+      .cookie("access_token", generateAuthToken(user._id, user.name, user.lastName, user.email, user.isAdmin), {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict"
+      })
+        .status(201)
+        .json({
+          success: "User created",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
     }
-
-  } catch (error) {
-    next(error)
+  } catch (err) {
+    next(err);
   }
-}
+};
 
-module.exports = {getUsers, registerUser};
-
+module.exports = { getUsers, registerUser };
