e85b9f6245c31ecc353f02c1bb54894803774ec1
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2022-11-07 17:36:12 -0500

    Add google maps
    Use address as props
     <GoogleMaps address={"15 Roehampton Ave, Toronto, ON M4P 0C2"}  />

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import HomePageComponent from "./components/HomePageComponent";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import GoogleMaps from "../utils/GoogleMaps";
 
 const getBestsellers = async () => {
     const { data } = await axios.get("/api/products/bestsellers");
@@ -11,7 +12,12 @@ const HomePage = () => {
 
     const { categories } = useSelector((state) => state.getCategories);
 
-  return <HomePageComponent categories={categories} getBestsellers={getBestsellers} />;
+  return (
+  <div>
+    <GoogleMaps address={"15 Roehampton Ave, Toronto, ON M4P 0C2"}/>
+    <HomePageComponent categories={categories} getBestsellers={getBestsellers} />
+  </div>
+  );
 };
 
 export default HomePage;
